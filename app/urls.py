453131c7fb1cc899bb1e10f67d502da280f22663
friveramm <134987490+friveramm@users.noreply.github.com>
2025-05-06 23:03:24 -0400

    06-05-25 Ingresar y rebajar stock

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from.views import home, agregar, lista, sucursales_y_stock
+from.views import home, agregar, sucursales_y_stock, agregar_stock, rebajar_stock
 
 # importar nombre puesto en def de la pagian desde "views"
 #   path('nombre de la pagina/', nombre_de_la_pagina, name= nombre de la pagina(esto es para el direcionamiento con botones))
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', home, name="home"),
     path('agregar/',agregar,name="agregar"),
     path('lista/', sucursales_y_stock, name="lista"),
+    path('stock/agregar/<int:stock_id>/', agregar_stock, name='agregar_stock'),
+    path('stock/rebajar/<int:stock_id>/', rebajar_stock, name='rebajar_stock'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Local
+from .models import Local, Stock
 
 # Create your views here.
 
@@ -10,10 +10,21 @@ def home(request):
     return render(request, 'app/home.html')
 def agregar(request):
     return render (request, 'crud/agregar.html')
-def lista(request):
-    return render (request, 'crud/lista.html')
 
 # Esto carga cada Local y sus Stock relacionados, incluyendo cada Producto asociado
 def sucursales_y_stock(request):
     locales = Local.objects.prefetch_related('stocks__producto').all()
     return render(request, 'crud/lista.html', {'locales': locales})
+
+def agregar_stock(request, stock_id):
+    stock = get_object_or_404(Stock, id=stock_id)
+    stock.cantidad += 1
+    stock.save()
+    return redirect('lista')
+
+def rebajar_stock(request, stock_id):
+    stock = get_object_or_404(Stock, id=stock_id)
+    if stock.cantidad > 0:
+        stock.cantidad -= 1
+        stock.save()
+    return redirect('lista')
